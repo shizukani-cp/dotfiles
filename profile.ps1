a9fd21658b1835ddff9e9236364edbd6f6c42cca
shizukani-cp <157582247+shizukani-cp@users.noreply.github.com>
2025-01-20 19:18:18 +0900

    profile.ps1を追加

---
+++ profile.ps1
@@ -0,0 +1,70 @@
+Remove-Alias -Name cat
+# Remove-Alias -Name cd
+Remove-Alias -Name chdir
+Remove-Alias -Name copy
+Remove-Alias -Name cp
+# Remove-Alias -Name curl
+Remove-Alias -Name del
+Remove-Alias -Name diff -Force
+Remove-Alias -Name echo
+Remove-Alias -Name ls
+Remove-Alias -Name md
+Remove-Alias -Name mv
+# Remove-Alias -Name wget
+Remove-Alias -Name rm
+set LANG=ja_JP.UTF-8
+set LESSCHARSET=utf-8
+
+function hb($prjname) {
+  cd "C:/Users/proch/Desktop/homebrew/homebrew/$prjname"  # このhomebrewはmacのパッケージ管理マネージャーとは関係なし
+  if (Test-Path -Path "./.venv") {
+    ./.venv/Scripts/activate
+  }
+}
+
+# function astnt() {
+  # aider --model gemini/gemini-1.5-pro-latest $args
+# }
+
+function Edit-Profile {
+  hx C:/Users/proch/Documents/PowerShell/profile.ps1
+}
+
+function Add-License {
+  param (
+    [string]$msg = "ライセンスを追加"
+  )
+  Write-Output(@"
+MIT License
+
+Copyright (c) {0} shizukani-cp
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"@ -f (Get-Date).Year ) > LICENSE.txt
+  git add LICENSE.txt
+  git commit -m "${msg}"
+}
+
+function prompt{
+    Write-Host "PS " -nonewline -foregroundcolor Yellow
+    Write-Host (Split-Path -Path (Get-Location) -Leaf) -nonewline -foregroundcolor White
+    Write-Host " >" -nonewline -foregroundcolor Yellow
+
+    return " "
+}
