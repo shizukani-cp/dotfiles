88b1f7121b9143eec7d5d3fde72ddad71d18922f
shizukani-cp <157582247+shizukani-cp@users.noreply.github.com>
2025-03-11 18:46:58 +0900

    helixのインストール方法を変更

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
-# curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/init.sh | sh
+#!/usr/bin/bash
+# curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/init.sh | bash
+read -p '(aarch64 or x86_64)' ARC
+read -p 'helix version: ') HX_VER
 sudo apt-get update
 sudo apt-get install git-all
 curl https://sh.rustup.rs -sSf | sh
 curl -LsSf https://astral.sh/uv/install.sh | sh
 sudo apt-get install software-properties-common
 sudo apt-get install build-essential
-sudo apt-get update
-sudo add-apt-repository ppa:maveonair/helix-editor
-sudo apt-get update
-sudo apt-get install helix
+curl -SsSf "https://github.com/helix-editor/helix/releases/download/$HX_VER/helix-$HX_VER-$ARC-linux.tar.xz" > helix.tar.xz
+tar xJf helix.tar.xz -O "helix-$HX_VAR-$ARC-linux/hx" > /usr/bin/hx
+rm helix.tar.xz
