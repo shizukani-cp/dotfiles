488373426acf47185e3ed64a9e8a91699b7007e6
shizukani-cp <157582247+shizukani-cp@users.noreply.github.com>
2025-03-11 17:44:54 +0900

    apt-getを使うよう変更

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/init.sh | sh
-sudo apt update
-sudo apt install git-all
+sudo apt-get update
+sudo apt-get install git-all
 curl https://sh.rustup.rs -sSf | sh
 curl -LsSf https://astral.sh/uv/install.sh | sh
-sudo apt install software-properties-common
-sudo apt install build-essential
-sudo apt update
+sudo apt-get install software-properties-common
+sudo apt-get install build-essential
+sudo apt-get update
 sudo add-apt-repository ppa:maveonair/helix-editor
-sudo apt update
-sudo apt install helix
+sudo apt-get update
+sudo apt-get install helix
