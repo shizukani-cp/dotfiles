3c21a1d6d9c496a6603ab2a3ed84db7c99b7a194
shizukani-cp <157582247+shizukani-cp@users.noreply.github.com>
2025-03-12 06:58:20 +0900

    インタラクティブな部分のあるuvのインストールを最初に行うよう変更

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/init.sh | bash
 read -p '(aarch64 or x86_64)' ARC
 read -p 'helix version: ') HX_VER
+curl -LsSf https://astral.sh/uv/install.sh | sh
 sudo apt-get update
 sudo apt-get install git-all
 curl https://sh.rustup.rs -sSf | sh
-curl -LsSf https://astral.sh/uv/install.sh | sh
 sudo apt-get install software-properties-common
 sudo apt-get install build-essential
 curl -SsSf "https://github.com/helix-editor/helix/releases/download/$HX_VER/helix-$HX_VER-$ARC-linux.tar.xz" > helix.tar.xz
