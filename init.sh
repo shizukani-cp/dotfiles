1c2a78c9a3e279115e4ad9b7ef0eda246e0e936d
shizukani-cp <to.maimai0606@gmail.com>
2025-03-12 14:36:30 +0900

    helixのインストール方法を戻した

--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/init.sh | bash
-read -p '(aarch64 or x86_64)' ARC
-read -p 'helix version: ') HX_VER
 curl https://sh.rustup.rs -sSf | sh # rustup
 curl -LsSf https://astral.sh/uv/install.sh | sh # uv
 sudo apt-get update
 sudo apt-get install git-all # git
 sudo apt-get install build-essential # gcc and other
-curl -SsSf "https://github.com/helix-editor/helix/releases/download/$HX_VER/helix-$HX_VER-$ARC-linux.tar.xz" > helix.tar.xz
-tar xJf helix.tar.xz -O "helix-$HX_VAR-$ARC-linux/hx" > /usr/bin/hx # helix editor
-rm helix.tar.xz
+sudo apt-get update
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt-get update
+sudo apt-get install helix
