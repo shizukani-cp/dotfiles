e10857a39b9556cc8a51471b5bb1aa2dbda30512
shizukani-cp <shizukani.cp@gmail.com>
2025-03-19 07:11:25 +0900

    インタラクティブな要素を頑張って止めた

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/init.sh | bash
-curl https://sh.rustup.rs -sSf | sh # rustup
+curl https://sh.rustup.rs -sSf | sh -- -y # rustup
 curl -LsSf https://astral.sh/uv/install.sh | sh # uv
 sudo apt-get update
 sudo apt-get install git-all -y # git
@@ -8,4 +8,4 @@ sudo apt-get install build-essential -y # gcc and other
 sudo apt-get update
 sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt-get update
-sudo apt-get install -y helix
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y helix
