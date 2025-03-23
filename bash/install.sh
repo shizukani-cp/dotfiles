9506a80d7ef8ea08d4ae61be26874135521087d9
shizukani-cp <shizukani.cp@gmail.com>
2025-03-24 07:04:26 +0900

    install.shもneovim仕様に変更

--- bash/install.sh
+++ bash/install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/bash/install.sh | bash
+NVIM_VERSION="10.0.4"
 curl https://sh.rustup.rs -sSf | sh -s -- -y # rustup
 curl -LsSf https://astral.sh/uv/install.sh | sh # uv
 . "$HOME/.cargo/env"
 sudo apt-get update
 sudo apt-get install git-all -y # git
 sudo apt-get install build-essential -y # gcc and other
-sudo apt-get update
-echo | sudo add-apt-repository ppa:maveonair/helix-editor
-sudo apt-get update
-sudo apt-get install -y helix
-mkdir ~/.config/helix/
-curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/helix/config.toml > ~/.config/helix/config.toml
-curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/helix/language.toml > ~/.config/helix/language.toml
+wget https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux-x86_64.tar.gz -O ~/.local/bin/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+rm -rf nvim-linux64.tar.gz
+mkdir -p ~/.config/nvim/lua/plugins/
+curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/neovim/init.lua > ~/.config/nvim/init.lua
+curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/neovim/lua/plugins/lazy.lua > ~/.config/nvim/lua/plugins/lazy.lua
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh # oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
