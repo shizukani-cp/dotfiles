d909619e8a05b7c8f6903fd5bb5ee2ea0f6a5c03
shizukani-cp <shizukani.cp@gmail.com>
2025-03-23 15:43:19 +0900

    いろいろ表示されるようにした

--- neovim/lua/plugins/lazy.lua
+++ neovim/lua/plugins/lazy.lua
@@ -11,6 +11,13 @@ return {
         dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
         config = function()
             require("neo-tree").setup({
+                filesystem = {
+                    filtered_items = {
+                        visible = true,
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                    },
+                },
                 window = {
                     position = "float",
                     width = 40,
