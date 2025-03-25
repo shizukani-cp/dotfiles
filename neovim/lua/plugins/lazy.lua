5bafe03a9b48f370c5c6f892b8d5cc3934835341
shizukani-cp <shizukani.cp@gmail.com>
2025-03-25 14:47:32 +0900

    lualine魔改造した

--- neovim/lua/plugins/lazy.lua
+++ neovim/lua/plugins/lazy.lua
@@ -144,7 +144,29 @@ return {
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function ()
             require('lualine').setup({
-                options = { theme = "onedark" }
+                options = {
+                    theme = "onedark",
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch'},
+                    lualine_c = {
+                        {
+                            'diff',
+                            symbols = {added = ' ', modified = ' ', removed = ' '},
+                        },
+                        {
+                            'diagnostics',
+                             symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '}
+                        },
+                    },
+                    lualine_x = {'encoding'},
+                    lualine_y = {
+                        'filetype',
+                        { 'filename', path=1 }
+                    },
+                    lualine_z = {'location'}
+                },
             })
         end,
     },
