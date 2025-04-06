2c1fd10b1a65c7a3fde14b7aff0d2359324f99ae
shizukani-cp <shizukani.cp@gmail.com>
2025-04-06 18:24:07 +0900

    Comment.nvimからmini.commentに乗り換えた

--- neovim/lua/plugins/Comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'numToStr/Comment.nvim',
-    opts = {
-        toggler = {
-            line = "<Space>c",
-            block = "<Space>C",
-        },
-    }
-}
---
+++ neovim/lua/plugins/mini_comment.lua
@@ -0,0 +1,12 @@
+return {
+    'echasnovski/mini.comment',
+    version = false,
+    lazy = true,
+    opts = {
+        mappings = {
+            comment_line = "<Space>c",
+            comment_visual = "<Space>C",
+            textObject = "<Space>c",
+        },
+    },
+}
