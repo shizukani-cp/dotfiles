f9b512f40268c4553dc5da27902c1b6f1af1a37f
shizukani-cp <shizukani.cp@gmail.com>
2025-04-07 07:30:39 +0900

    neo-treeからmini.filesに乗り換えた

---
+++ neovim/lua/plugins/mini_files.lua
@@ -0,0 +1,14 @@
+return {
+    'echasnovski/mini.files',
+    version = false,
+    keys = {
+        {
+            "<Space>f",
+            function ()
+                require("mini.files").open()
+            end,
+            mode = { "n", },
+            silent = true,
+        },
+    },
+}
