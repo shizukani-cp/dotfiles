7f3d4b9c76f413e485f9e45d29cbb5bd4943fda1
shizukani-cp <shizukani.cp@gmail.com>
2025-04-09 17:01:59 +0900

    ~/.cacheにdotfilesをgit cloneしてそこからcpするように変更

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64
 mv nvim-linux-x86_64.appimage ~/appimages/nvim.appimage
 chmod a+x ~/appimages/nvim.appimage
 mkdir -p ~/.config/nvim/lua/plugins/
-curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/neovim/init.lua > ~/.config/nvim/init.lua
-curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/neovim/lua/plugins/lazy.lua > ~/.config/nvim/lua/plugins/lazy.lua
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh # oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
@@ -21,6 +19,9 @@ sudo apt-get install -y unzip
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
 rm ~/.poshthemes/themes.zip
-curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/bash/my-theme.omp.json > ~/.poshthemes/my-theme.omp.json
-curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/bash/.shfunc > ~/.shfunc
+git clone https://github.com/shizukani-cp/dotfiles.git ~/.cache/dotfiles
+cp ~/.cache/dotfiles/bash/my-theme.omp.json ~/.poshthemes/my-theme.omp.json
+cp ~/.cache/dotfiles/bash/.shfunc ~/.shfunc
+cp -r ~/.cache/dotfiles/neovim/ ~/.config/nvim/
+rm -rf ~/.cache/dotfiles/
 echo '. "$HOME/.shfunc"' >> ~/.bashrc
