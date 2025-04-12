d4f9a4a98702caa095bdbc44d85d390725e969b0
shizukani-cp <shizukani.cp@gmail.com>
2025-04-12 15:46:37 +0900

    skkeletonを追加

---
+++ neovim/lua/plugins/skkeleton.lua
@@ -0,0 +1,37 @@
+return {
+    'vim-skk/skkeleton',
+    dependencies = {
+        "vim-denops/denops.vim",
+        "Shougo/ddc.vim",
+        "Shougo/ddc-ui-native",
+    },
+    config = function()
+        vim.fn["skkeleton#config"]({
+            globalDictionaries = { '~/.config/eskk/SKK-JISYO.L' },
+            eggLikeNewline = true,
+        })
+        vim.fn["ddc#custom#patch_global"]('ui', 'native')
+        vim.fn["ddc#custom#patch_global"]('sources', { 'skkeleton' })
+        vim.fn["ddc#custom#patch_global"]({
+            sourceOptions = {
+                ["skkeleton"] = {
+                    mark = 'skkeleton',
+                    matchers = {},
+                    sorters = {},
+                    converters = {},
+                    isVolatile = true,
+                    minAutoCompleteLength = 1,
+                },
+            }
+        })
+        vim.fn["ddc#enable"]()
+    end,
+    keys = {
+        {
+            "<C-s>",
+            "<Plug>(skkeleton-enable)",
+            mode = { "i", "c" },
+            desc = "skkeleton enable",
+        },
+    },
+}
