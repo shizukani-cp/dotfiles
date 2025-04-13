6155be039d2085bb7960e665a4784be8d0486ab5
shizukani-cp <shizukani.cp@gmail.com>
2025-04-14 04:19:47 +0900

    <Space>Pよく使ってたなあ

--- neovim/init.lua
+++ neovim/init.lua
@@ -38,5 +38,6 @@ require("lazy").setup({
 
 vim.api.nvim_set_keymap('v', '<Space>y', 'y', { noremap = true, silent = true, desc = "Yank to clipboard" })
 vim.api.nvim_set_keymap('n', '<Space>p', 'p', { noremap = true, silent = true, desc = "Paste from clipboard" })
+vim.api.nvim_set_keymap('n', '<Space>P', 'P', { noremap = true, silent = true, desc = "Paste from clipboard" })
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
