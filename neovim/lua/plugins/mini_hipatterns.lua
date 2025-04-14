3009ee00aa6b45148fee184013f791c882c898f4
shizukani-cp <shizukani.cp@gmail.com>
2025-04-14 18:16:40 +0900

    画面上に色表示するのってminiでもあったんだ

---
+++ neovim/lua/plugins/mini_hipatterns.lua
@@ -0,0 +1,10 @@
+return {
+    'echasnovski/mini.hipatterns',
+    version = false,
+    lazy = true,
+    opts = {
+        highlighters = {
+            hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+        },
+    },
+}
--- neovim/lua/plugins/nvim-colorizer.lua
+++
@@ -1,5 +0,0 @@
-return {
-    'norcalli/nvim-colorizer.lua',
-    lazy = true,
-    opts = {}
-}
