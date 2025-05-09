53a386e4397b47f71b1d77e0263a7e07f9762034
shizukani-cp <shizukani.cp@gmail.com>
2025-05-09 18:26:24 +0900

    LSP関連で修正

--- neovim/lua/plugins/nvim-lspconfig.lua
+++ neovim/lua/plugins/nvim-lspconfig.lua
@@ -18,7 +18,14 @@ return {
                     Lua = {
                         runtime = { version = 'LuaJIT' },
                         diagnostics = { globals = {'vim'} },
-                        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+                        workspace = {
+                            library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
+                                "${3rd}/luv/library",
+                                "${3rd}/busted/library",
+                                "${3rd}/luassert/library",
+                            }),
+                            checkThirdParty = "Disable",
+                        },
                     },
                 },
             },
