bf26facb4ca356b03379dc35d00b6d0e370312e7
shizukani-cp <shizukani.cp@gmail.com>
2025-05-24 14:45:23 +0900

    aider関連を改造

--- neovim/lua/plugins/aider.lua
+++ neovim/lua/plugins/aider.lua
@@ -6,7 +6,7 @@ return {
     keys = {
         {
             "<Space>l",
-            "<CMD>AiderOpen --model openrouter/mistralai/mistral-7b-instruct:free<CR>",
+            "<CMD>AiderOpen --model openrouter/mistralai/mistral-7b-instruct:free --dark-mode --read ~/.aiderprmt<CR>",
             desc = "Open Aider",
             mode = "n",
         },
