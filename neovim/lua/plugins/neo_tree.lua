22fd4cc30aeec44b7231f744b9ab71254989248f
shizukani-cp <shizukani.cp@gmail.com>
2025-05-24 14:20:05 +0900

    mini.filesがやっぱり使いずらい

---
+++ neovim/lua/plugins/neo_tree.lua
@@ -0,0 +1,30 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim"
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+        },
+        window = {
+            position = "float",
+            width = 40,
+            height = 30,
+        },
+    },
+    keys = {
+        {
+            "<Space>f",
+            "<CMD>Neotree<CR>",
+            mode = { "n", },
+            silent = true,
+        },
+    },
+}
