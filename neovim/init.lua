85eb3dc3e3545a2f8fe80547c7627ecc16187e3f
shizukani-cp <shizukani.cp@gmail.com>
2025-06-09 16:11:05 +0900

    インデントサイズを変更

--- neovim/init.lua
+++ neovim/init.lua
@@ -4,16 +4,16 @@ vim.o.fileencoding = 'utf-8'
 vim.o.fileencodings = "utf-8,cp932,euc-jp,sjis"
 
 vim.g.clipboard = {
-  name = 'win32yankClipboard',
-  copy = {
-    ['+'] = 'win32yank.exe -i --crlf',
-    ['*'] = 'win32yank.exe -i --crlf',
-  },
-  paste = {
-    ['+'] = 'win32yank.exe -o --lf',
-    ['*'] = 'win32yank.exe -o --lf',
-  },
-  cache_enabled = 1,
+    name = 'win32yankClipboard',
+    copy = {
+        ['+'] = 'win32yank.exe -i --crlf',
+        ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+        ['+'] = 'win32yank.exe -o --lf',
+        ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 1,
 }
 
 vim.opt.number = true
@@ -25,35 +25,35 @@ vim.opt.scrolloff = 4
 
 local term_grp = vim.api.nvim_create_augroup("NeovimTerminal", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
-  group = term_grp,
-  pattern = "*",
-  callback = function()
-    vim.opt_local.number = false
-    vim.cmd("startinsert")
-  end,
+    group = term_grp,
+    pattern = "*",
+    callback = function()
+        vim.opt_local.number = false
+        vim.cmd("startinsert")
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "html", "css", "js", "ts" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-  end,
+    pattern = { "html", "css", "js", "ts" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
