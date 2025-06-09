4cbea0b0564f4d54ae51e4a40764a5d0da203709
shizukani-cp <shizukani.cp@gmail.com>
2025-06-09 15:58:03 +0900

    ファイルエンコーディング回りで変更

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,6 +1,7 @@
 vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.o.encoding = 'utf-8'
+vim.o.fileencoding = 'utf-8'
+vim.o.fileencodings = "utf-8,cp932,euc-jp,sjis"
 
 vim.g.clipboard = {
   name = 'win32yankClipboard',
