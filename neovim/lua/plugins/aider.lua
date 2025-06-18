b91e5ea84921c27a126b9787df36c047ab28c73b
shizukani-cp <shizukani.cp@gmail.com>
2025-06-18 15:59:19 +0900

    モデル変えよ

--- neovim/lua/plugins/aider.lua
+++ neovim/lua/plugins/aider.lua
@@ -6,7 +6,7 @@ return {
     keys = {
         {
             "<Space>l",
-            "<CMD>AiderOpen --model openrouter/mistralai/mistral-7b-instruct:free --dark-mode --read ~/.aiderprmt<CR>",
+            "<CMD>AiderOpen --model openrouter/mistralai/devstral-small:free --dark-mode --read ~/.aiderprmt<CR>",
             desc = "Open Aider",
             mode = "n",
         },
