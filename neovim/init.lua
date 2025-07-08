c5743d5f7373b97d92d164ca524d83da0b81b3a1
shizukani-cp <shizukani.cp@gmail.com>
2025-07-08 16:30:52 +0900

    LSP関連を分離

--- neovim/init.lua
+++ neovim/init.lua
@@ -66,6 +66,7 @@ require("lazy").setup({
     },
     spec = {
         { import = "plugins" },
+        { import = "plugins.lsp" },
     },
     defaults = {
         lazy = false,
--- neovim/lua/plugins/lsp/nvim-lspconfig.lua
+++ neovim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,15 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "hrsh7th/nvim-cmp",
-        "saghen/blink.cmp",
         "mason-org/mason.nvim",
         "mason-org/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
         "onsails/lspkind-nvim",
-        "uga-rosa/cmp-skkeleton",
     },
     config = function()
         local lsp_servers = {
@@ -54,8 +49,6 @@ return {
             ensure_installed = installed_servers,
         })
 
-        local lspconfig = require("lspconfig")
-        local cmp = require("cmp")
         local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         local on_attach = function(client, bufnr)
@@ -85,54 +78,6 @@ return {
                 end
             end,
         })
-
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                require("luasnip").lsp_expand(args.body)
-            end,
-        },
-        mapping = {
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<CR>'] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
-            }),
-            ['<Tab>'] = cmp.mapping.select_next_item(),
-            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-            ['<C-k>'] = cmp.mapping.select_prev_item(),
-            ['<C-j>'] = cmp.mapping.select_next_item(),
-            ['<C-e>'] = cmp.mapping.close(),
-        },
-        sources = {
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-            { name = "buffer" },
-            { name = "path" },
-            { name = "skkeleton" },
-        },
-        formatting = {
-            fields = { "menu", "abbr", "kind" },
-            format = function(entry, vim_item)
-                local menu_icon = {
-                    nvim_lsp = "lsp",
-                    luasnip = "snip",
-                    buffer = "buf",
-                    path = "path",
-                    skkeleton = "skk",
-                }
-                vim_item.menu = menu_icon[entry.source.name] or "?"
-                return vim_item
-            end,
-        },
-        experimental = {
-            ghost_text = {
-                hl_group = "LspCodeLens",
-            },
-        },
-    })
-end,
+    end,
 }
 
