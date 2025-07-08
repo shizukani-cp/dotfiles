d2f38c173d268d2ee3072670982483e8c658d81d
shizukani-cp <shizukani.cp@gmail.com>
2025-07-08 16:32:43 +0900

    カーソルが動くとかっこいいよね

---
+++ neovim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        stiffness = 0.3,
+        trailing_stiffness = 0.1,
+        trailing_exponent = 5,
+        hide_target_hack = true,
+        gamma = 1,
+    }
+}
