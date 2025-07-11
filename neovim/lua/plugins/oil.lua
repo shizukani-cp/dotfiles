ad327537ab64c99f5ab44922a6a75cc2458a9b98
shizukani-cp <shizukani.cp@gmail.com>
2025-07-11 13:11:59 +0900

    ファイラーにgitのステータスは欲しい

--- neovim/lua/plugins/oil.lua
+++ neovim/lua/plugins/oil.lua
@@ -1,9 +1,18 @@
 return {
     "stevearc/oil.nvim",
-    dependencies = { "echasnovski/mini.icons" },
-    config = function ()
-        require("oil").setup({})
-    end,
+    dependencies = {
+        "echasnovski/mini.icons",
+        {
+            "FerretDetective/oil-git-signs.nvim",
+            opts = {},
+        },
+    },
+    opts = {
+        win_options = {
+            signcolumn = "yes:2",
+            statuscolumn = "",
+        },
+    },
     keys = {
         {
             "<Space>f",
