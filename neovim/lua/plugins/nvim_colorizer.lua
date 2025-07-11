280cdb05857b459b28c283bc0a0cdfa01e7cf63b
shizukani-cp <shizukani.cp@gmail.com>
2025-07-11 13:23:35 +0900

    プログラミング中でも色はついてて欲しい

---
+++ neovim/lua/plugins/nvim_colorizer.lua
@@ -0,0 +1,17 @@
+return {
+    "norcalli/nvim-colorizer.lua",
+    opts = {
+        css = {
+            css = true,
+            css_fn = true,
+        },
+        html = {
+            css = true,
+            css_fn = true,
+        },
+        javascript = {
+            css = true,
+            css_fn = true,
+        }
+    }
+}
