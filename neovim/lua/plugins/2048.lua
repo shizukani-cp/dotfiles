a42483691dd324d58343a17181b087ee1cfa5f98
shizukani-cp <shizukani.cp@gmail.com>
2025-07-11 15:14:09 +0900

    neovimで2048ができる...だと...

---
+++ neovim/lua/plugins/2048.lua
@@ -0,0 +1,12 @@
+return {
+    "NStefan002/2048.nvim",
+    cmd = "Play2048",
+    opts = {
+        keys = {
+            up = "<Up>",
+            down = "<Down>",
+            left = "<Left>",
+            right = "<Right>",
+        }
+    }
+}
