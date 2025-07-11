c858cfa7c8ef973ccec1b4702e358aa39e48bbea
shizukani-cp <shizukani.cp@gmail.com>
2025-07-11 12:05:12 +0900

    カーソルの残像で文字が見えないのは困る

--- neovim/lua/plugins/smear-cursor.lua
+++ neovim/lua/plugins/smear-cursor.lua
@@ -1,10 +1,8 @@
 return {
     "sphamba/smear-cursor.nvim",
     opts = {
-        stiffness = 0.3,
-        trailing_stiffness = 0.1,
-        trailing_exponent = 5,
-        hide_target_hack = true,
-        gamma = 1,
+        stiffness = 0.5,
+        trailing_stiffness = 0.49,
+        never_draw_over_target = false,
     }
 }
