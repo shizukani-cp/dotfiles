fe91e6e4fc517e7a7ab2d94432d68920c47d0d96
shizukani-cp <shizukani.cp@gmail.com>
2025-07-12 12:49:13 +0900

    blink.cmpからddc.vimへ移行。初のShougowere!!!!!!!!

---
+++ neovim/lua/plugins/lsp/ddc.lua
@@ -0,0 +1,66 @@
+return {
+    "Shougo/ddc.vim",
+    dependencies = {
+        "vim-denops/denops.vim",
+        "Shougo/ddc-ui-native",
+        "Shougo/ddc-source-lsp",
+        "matsui54/ddc-source-buffer",
+        "LumaKernel/ddc-source-file",
+        "Shougo/ddc-filter-matcher_head",
+        "Shougo/ddc-filter-sorter_rank",
+        "tani/ddc-fuzzy",
+    },
+    config = function()
+        vim.fn["ddc#custom#set_global"]({
+            ui = "native",
+            sources = { "lsp", "buffer", "file", },
+            sourceOptions = {
+                _ = {
+                    matchers = { "matcher_fuzzy" },
+                    sorters = { "sorter_fuzzy" },
+                    converters = { "converter_fuzzy" },
+                },
+                lsp = {
+                    mark = "L",
+                    forceCompletionPattern = [[\.\w*|::\w*|->\w*]],
+                },
+                buffer = {
+                    mark = "B",
+                },
+                file = {
+                    mark = "F",
+                },
+            },
+            sourceParams = {
+                lsp = {
+                    debounce = 500,
+                },
+            },
+        })
+
+        vim.keymap.set(
+            'i', '<C-j>',
+            function()
+                return vim.fn.pumvisible()==1 and '<C-n>' or vim.fn['ddc#map#manual_complete']()
+            end,
+            { expr=true, silent=true, desc='ddc candidate next' }
+        )
+        vim.keymap.set(
+            'i', '<C-k>',
+            function()
+                return vim.fn.pumvisible()==1 and '<C-p>' or ''
+            end,
+            { expr=true, silent=true, desc='ddc candidate prev' }
+        )
+
+        vim.fn["ddc#enable"]()
+    end,
+    keys = {
+        {
+            "<C-Space>",
+            function ()
+                vim.fn["ddc#pum#open()"]()
+            end,
+        }
+    }
+}
--- neovim/lua/plugins/skkeleton.lua
+++ neovim/lua/plugins/skkeleton.lua
@@ -3,11 +3,31 @@ return {
     event = "InsertEnter",
     dependencies = {
         "vim-denops/denops.vim",
+        "Shougo/ddc.vim",
     },
     config = function()
         vim.fn["skkeleton#config"]({
             globalDictionaries = { '~/.config/eskk/SKK-JISYO.L' },
             eggLikeNewline = true,
+        })
+
+        -- ddc.vim の設定
+        -- skkeleton が有効なとき (filetype=skkeleton) の ddc の設定
+        vim.fn["ddc#custom#patch_filetype"]("skkeleton", {
+            sources = { "skkeleton" },
+        })
+
+        vim.fn["ddc#custom#patch_global"]({
+            sourceOptions = {
+                ["skkeleton"] = {
+                    mark = 'skkeleton',
+                    matchers = {},
+                    sorters = {},
+                    converters = {},
+                    isVolatile = true,
+                    minAutoCompleteLength = 1,
+                },
+            }
         })
     end,
     keys = {
