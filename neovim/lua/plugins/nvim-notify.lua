ba92c442426ac51370407a7e872a9822d13ea938
shizukani-cp <shizukani.cp@gmail.com>
2025-08-04 15:45:11 +0900

    nvim-notifyは即時ロードしないといけなそう

--- neovim/lua/plugins/nvim-notify.lua
+++ neovim/lua/plugins/nvim-notify.lua
@@ -7,10 +7,4 @@ manager.add({
         require("notify").setup()
     end
 })
-vim.api.nvim_create_autocmd("VimEnter", {
-    group = vim.api.nvim_create_augroup("nvim-notify_load", { clear = true }),
-    pattern = "*",
-    callback = function ()
-        manager.load("nvim-notify")
-    end
-})
+manager.load("nvim-notify")
