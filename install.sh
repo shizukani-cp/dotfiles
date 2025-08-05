6c79315fe75fde46e675ce14ff20820098978903
shizukani-cp <shizukani.cp@gmail.com>
2025-08-05 12:59:49 +0900

    改行開けた方が見やすいよね

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/install.sh | bash
 curl https://sh.rustup.rs -sSf | sh -s -- -y # rustup
 curl -LsSf https://astral.sh/uv/install.sh | sh # uv
+
 sudo apt-get update
 sudo apt-get install git-all -y # git
 sudo apt-get install build-essential -y # gcc and other
@@ -12,6 +13,7 @@ sudo apt-get install fd-find -y # fd
 sudo npm install -g @google/gemini-cli # gemini-cli
 uv tool install debugpy # debugpy
 sudo snap install nvim --classic
+
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh # oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
@@ -20,12 +22,17 @@ sudo apt-get install -y unzip
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
 rm ~/.poshthemes/themes.zip
+
 git clone https://github.com/shizukani-cp/dotfiles.git ~/.cache/dotfiles
 cp ~/.cache/dotfiles/bash/my-theme.omp.json ~/.poshthemes/my-theme.omp.json
+
 cp ~/.cache/dotfiles/bash/.shfunc ~/.shfunc
 cp -r ~/.cache/dotfiles/neovim/ ~/.config/nvim/
+
 mkdir ~/.diary
 git clone https://github.com/shizukani-cp/diary.git ~/.diary
+
 rm -rf ~/.cache/dotfiles/
+
 echo '. "$HOME/.shfunc"' >> ~/.bashrc
 mkdir ~/.Trash/
