cf6bb51f7ecd2eab83ae65054764a09fce0f2148
shizukani-cp <shizukani.cp@gmail.com>
2025-08-28 13:25:08 +0900

    なんかみんな使ってるtoggleterm.nvimを導入した

---
+++ neovim/lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+local manager = require("utils.manager")
+
+manager.add({
+    id = "toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim",
+    config = function ()
+        require("toggleterm").setup({
+            open_mapping = [[<Space>/]],
+            hide_numbers = true,
+            close_on_exit = true,
+            direction = "tab"
+        })
+    end
+})
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = vim.api.nvim_create_augroup("toggleterm.nvim_load", { clear = true }),
+    pattern = "*",
+    callback = function ()
+        manager.load("toggleterm.nvim")
+    end
+})
