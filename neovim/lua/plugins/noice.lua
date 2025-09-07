f895d7bea370b0f28ffcd1441337d02509fbd5b4
shizukani-cp <shizukani.cp@gmail.com>
2025-09-07 11:11:54 +0900

    コマンドラインモードになるまでメッセージが表示されない原因これだったのか

--- neovim/lua/plugins/noice.lua
+++ neovim/lua/plugins/noice.lua
@@ -12,7 +12,7 @@ manager.add({
         require("noice").setup()
     end
 })
-vim.api.nvim_create_autocmd("CmdlineEnter", {
+vim.api.nvim_create_autocmd("VimEnter", {
     group = vim.api.nvim_create_augroup("noice.nvim_load", { clear = true }),
     pattern = "*",
     callback = function ()
