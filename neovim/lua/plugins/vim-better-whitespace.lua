bd94366b3686fef935fc7b56745bd0b6546de889
shizukani-cp <shizukani.cp@gmail.com>
2025-10-01 16:24:39 +0900

    入れたあとにこれフォーマッターでよくね？って思ったけどまぁ入れておく

---
+++ neovim/lua/plugins/vim-better-whitespace.lua
@@ -0,0 +1,8 @@
+local manager = require("utils.manager")
+
+manager.add({
+    id = "vim-better-whitespace",
+    url = "https://github.com/ntpeters/vim-better-whitespace",
+})
+
+manager.load("vim-better-whitespace")
