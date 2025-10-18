1fb0d443b0eb43bcbd0977c2d5bdb7b2f32d50db
shizukani-cp <shizukani.cp@gmail.com>
2025-10-18 14:56:14 +0900

    辞書追加

--- neovim/lua/plugins/skkeleton.lua
+++ neovim/lua/plugins/skkeleton.lua
@@ -11,6 +11,8 @@ manager.add({
         vim.fn["skkeleton#config"]({
             globalDictionaries = {
                 '~/.config/eskk/SKK-JISYO.L',
+                '~/.config/eskk/SKK-JISYO.assoc',
+                '~/.config/eskk/SKK-JISYO.fullname',
                 '~/.config/eskk/SKK-JISYO.geo',
                 '~/.config/eskk/SKK-JISYO.jinmei',
                 '~/.config/eskk/SKK-JISYO.propernoun',
