997f5c951ee0d6a1bf4ac170a0f5608410777f43
shizukani-cp <shizukani.cp@gmail.com>
2025-10-28 19:07:07 +0900

    textobject使ってみようかな(結局使わないかも)

---
+++ neovim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,51 @@
+local manager = require("utils.manager")
+local lazyload = require("utils.lazyload")
+
+manager.add({
+    id = "nvim-treesitter-textobjects",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+        "nvim-treesitter",
+    },
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["ab"] = "@block.outer",
+                        ["ib"] = "@block.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                    },
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v',
+                        ['@function.outer'] = 'V',
+                        ['@class.outer'] = '<c-v>',
+                    },
+                    include_surrounding_whitespace = true,
+                },
+                move = {
+                    enable = true,
+                    goto_next_start = {
+                        ["Jb"] = "@block.outer",
+                        ["Jc"] = "@class.outer",
+                        ["Jf"] = "@function.outer",
+                    },
+                    goto_previous_start = {
+                        ["Kb"] = "@block.outer",
+                        ["Kc"] = "@class.outer",
+                        ["Kf"] = "@function.outer",
+                    },
+                    set_jumps = true,
+                }
+            },
+        })
+        vim.keymap.set("n", "K", "<Nop>")
+    end
+})
+
+lazyload.event("VimEnter", "nvim-treesitter-textobjects")
