b0d0c01015265788e4d930ab58180015a2e97815
shizukani-cp <shizukani.cp@gmail.com>
2025-11-08 14:20:50 +0900

    textobject系のプラグインってまだあったんだ

---
+++ neovim/lua/plugins/nvim-various-textobjs.lua
@@ -0,0 +1,18 @@
+local lazyload = require("utils.lazyload")
+
+local function config()
+    require("various-textobjs").setup({
+        keymaps = {
+            useDefaults = true
+        }
+    })
+end
+
+return function(manager)
+    manager.add({
+        id = "nvim-various-textobjs",
+        url = "https://github.com/chrisgrieser/nvim-various-textobjs",
+        config = config,
+    })
+    lazyload.event("VimEnter", "nvim-various-textobjs")
+end
