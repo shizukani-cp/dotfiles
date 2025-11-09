8ad7b82a9249a2beb85296cdd2ecc57ed1d8a542
shizukani-cp <shizukani.cp@gmail.com>
2025-11-09 12:54:57 +0900

    ts_lsとdenols両方でエラー吐かれて発狂とかよくあるからどっちがどっちか分かるの地味に助かる

---
+++ neovim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,27 @@
+local lazyload = require("utils.lazyload")
+
+local function config()
+    require("tiny-inline-diagnostic").setup({
+        options = {
+            add_messages = {
+                display_count = true,
+            },
+            multilines = {
+                enabled = true,
+            },
+            show_source = {
+                enabled = true,
+            },
+        },
+    })
+    vim.diagnostic.config({ virtual_text = false })
+end
+
+return function(manager)
+    manager.add({
+        id = "tiny-inline-diagnostic.nvim",
+        url = "https://github.com/rachartier/tiny-inline-diagnostic.nvim",
+        config = config,
+    })
+    lazyload.event("VimEnter", "tiny-inline-diagnostic.nvim")
+end
