490b6057c6d86c8fab02404675ad24f58958e9c4
shizukani-cp <shizukani.cp@gmail.com>
2025-11-23 17:07:20 +0900

    hit_enterってToggleできた方が便利だな

--- neovim/lua/core/keymap.lua
+++ neovim/lua/core/keymap.lua
@@ -11,6 +11,7 @@ vim.keymap.set("n", "<Leader>en", function()
         vim.wo.number = true
     end
 end, { desc = "Relative/Absolute Line Numbers" })
+vim.keymap.set("n", "<Leader>eh", "<Cmd>ToggleHitEnter<Cr>", { desc = "Enable/Disable hit enter" })
 vim.keymap.set({ 'n', 'x' }, 'gg', 'gg^', { noremap = true, silent = true, desc = "Go to the top of the file" })
 vim.keymap.set({ 'n', 'x' }, 'G', 'G$', { noremap = true, silent = true, desc = "Go to the end of the file" })
 vim.keymap.set('n', 'o', 'o<Esc>', { noremap = true, silent = true, desc = "Add new line" })
--- neovim/lua/core/option.lua
+++ neovim/lua/core/option.lua
@@ -30,5 +30,21 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.scrolloff = 4
 vim.opt.relativenumber = true
-vim.opt.messagesopt:append("wait:0")
-vim.opt.messagesopt:remove("hit-enter")
+
+vim.g.hit_enter = false
+local function set_hit_enter()
+    if vim.g.hit_enter then
+        vim.opt.messagesopt:append("hit-enter")
+        vim.opt.messagesopt:remove("wait:0")
+    else
+        vim.opt.messagesopt:append("wait:0")
+        vim.opt.messagesopt:remove("hit-enter")
+    end
+    vim.g.hit_enter = not vim.g.hit_enter
+end
+
+vim.api.nvim_create_user_command("ToggleHitEnter", set_hit_enter, {
+    nargs = 0
+})
+
+set_hit_enter()
