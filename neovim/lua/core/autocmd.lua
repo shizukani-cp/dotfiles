ae087cec91bf92fc7118c29562fff0ec861aa0c5
shizukani-cp <shizukani.cp@gmail.com>
2025-11-23 17:10:52 +0900

    コマンド履歴削除プラグインでやる必要なかった
    [参考](https://blog.atusy.net/2023/07/24/vim-clean-history/)

--- neovim/lua/core/autocmd.lua
+++ neovim/lua/core/autocmd.lua
@@ -16,3 +16,15 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         vim.lsp.buf.format({ async = false })
     end,
 })
+
+local clean_history_grp = vim.api.nvim_create_augroup("CleanHistory", { clear = true })
+vim.api.nvim_create_autocmd("ModeChanged", {
+    pattern = "c:*",
+    group = clean_history_grp,
+    callback = function()
+        local cmd = vim.fn.histget(":", -1)
+        if cmd == "x" or cmd == "xa" or cmd:match("^w?q?a?!?$") then
+            vim.fn.histdel(":", -1)
+        end
+    end,
+})
