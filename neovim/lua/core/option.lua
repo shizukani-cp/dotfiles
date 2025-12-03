5dc00c6da4388ce1510b3a04c340afa86e1dd687
shizukani-cp <shizukani.cp@gmail.com>
2025-12-03 18:58:23 +0900

    クリップボード変だと思ったらWSL時代の遺物があった

--- neovim/lua/core/option.lua
+++ neovim/lua/core/option.lua
@@ -5,19 +5,6 @@ vim.o.fileencodings = "utf-8,cp932,euc-jp,sjis"
 vim.g.mapleader = " "
 vim.diagnostic.config({ virtual_text = true })
 
-vim.g.clipboard = {
-    name = 'win32yankClipboard',
-    copy = {
-        ['+'] = 'win32yank.exe -i --crlf',
-        ['*'] = 'win32yank.exe -i --crlf',
-    },
-    paste = {
-        ['+'] = 'win32yank.exe -o --lf',
-        ['*'] = 'win32yank.exe -o --lf',
-    },
-    cache_enabled = 1,
-}
-
 vim.o.helplang = "ja,en"
 vim.wo.cursorline = true
 vim.opt.foldmethod = "indent"
