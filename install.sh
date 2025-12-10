2513f72ce87b2d1d203dc3d95abb31fc181e7e2f
shizukani-cp <shizukani.cp@gmail.com>
2025-12-10 19:50:07 +0900

    てとりすたのしいね

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ npm install -g @google/gemini-cli # gemini-cli
 uv tool install debugpy # debugpy
 sudo snap install nvim --classic
 sudo snap install gh # GitHub CLI
+sudo snap install tetris-thefenriswolf # tetris
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
