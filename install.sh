99c466f5b3fdd30e372f6aba28955ce1085c1c78
shizukani-cp <shizukani.cp@gmail.com>
2025-12-17 15:04:01 +0900

    なんかタスクマネージャーとTUIを融合すると頭よさそうの2乗になるよね

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ uv tool install debugpy # debugpy
 sudo snap install nvim --classic # NeoVim
 sudo snap install gh # GitHub CLI
 sudo snap install tetris-thefenriswolf # tetris
+sudo snap install bottom # bottom
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
