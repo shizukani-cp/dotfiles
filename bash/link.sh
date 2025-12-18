54f532a05aa7609b456f727c10058445fe95adb1
shizukani-cp <shizukani.cp@gmail.com>
2025-12-18 14:14:59 +0900

    色々と事故ってたところを修正

--- bash/link.sh
+++ bash/link.sh
@@ -1,10 +1,10 @@
-local DOTFILE_PATHS="$HOME/workspace/dotfiles"
+DOTFILES_PATH="$HOME/workspace/dotfiles"
 
 if [ $(basename $(pwd -P)) = "dotfiles" ]; then
-  ln -s "$DOTFILES_PATH/neovim/" "$HOME/.config/nvim"
-  ln -s "$DOTFILES_PATH/bash/.shfunc" "$HOME/.shfunc"
-  ln -s "$DOTFILES_PATH/bash/.shopts" "$HOME/.shopts"
-  ln -s "$DOTFILES_PATH/.wezterm.lua" "$HOME/.wezterm.lua"
-  ln -s "$DOTFILES_PATH/.gitconfig" "$HOME/.gitconfig"
-  ln -s "$DOTFILES_PATH/bash/link.sh" "$HOME/.local/bin/link.sh"
+  ln -sf "$DOTFILES_PATH/nvim" "$HOME/.config/"
+  ln -sf "$DOTFILES_PATH/bash/.shfunc" "$HOME/.shfunc"
+  ln -sf "$DOTFILES_PATH/bash/.shopts" "$HOME/.shopts"
+  ln -sf "$DOTFILES_PATH/.wezterm.lua" "$HOME/.wezterm.lua"
+  ln -sf "$DOTFILES_PATH/.gitconfig" "$HOME/.gitconfig"
+  ln -sf "$DOTFILES_PATH/bash/link.sh" "$HOME/.local/bin/link.sh"
 fi
