afa958745664411fd66a012fb5d5410e137e8299
shizukani-cp <shizukani.cp@gmail.com>
2025-12-22 16:13:27 +0900

    出るのが脳死連打でいけるのいいね

---
+++ nvim/lua/plugins/in-and-out.lua
@@ -0,0 +1,14 @@
+local lazyload = require("manager.lazyload")
+
+local function config()
+
+end
+
+return function(manager)
+    manager.add({
+        id = "in-and-out.nvim",
+        url = "https://github.com/ysmb-wtsg/in-and-out.nvim",
+        config = config,
+    })
+    lazyload.key('i', '<C-CR>', function() require("in-and-out").in_and_out() end)
+end
