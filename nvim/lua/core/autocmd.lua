0f46b0502a4b4c33ffc1f0705e4c1f90cb018270
shizukani-cp <shizukani.cp@gmail.com>
2025-12-31 09:44:25 +0900

    別に変数にする必要ないのか

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -1,6 +1,5 @@
-local term_grp = vim.api.nvim_create_augroup("NeovimTerminal", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
-    group = term_grp,
+    group = vim.api.nvim_create_augroup("NeovimTerminal", { clear = true }),
     pattern = "*",
     callback = function()
         vim.opt_local.number = false
@@ -8,19 +7,17 @@ vim.api.nvim_create_autocmd("TermOpen", {
     end,
 })
 
-local format_on_save_grp = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
-    group = format_on_save_grp,
+    group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
     pattern = "*",
     callback = function()
         vim.lsp.buf.format({ async = false })
     end,
 })
 
-local clean_history_grp = vim.api.nvim_create_augroup("CleanHistory", { clear = true })
 vim.api.nvim_create_autocmd("ModeChanged", {
     pattern = "c:*",
-    group = clean_history_grp,
+    group = vim.api.nvim_create_augroup("CleanHistory", { clear = true }),
     callback = function()
         local cmd = vim.fn.histget(":", -1)
         if cmd == "x" or cmd == "xa" or cmd:match("^w?q?a?!?$") then
