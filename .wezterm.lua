eeaa53a0bc9b10c061c5cd637f548d1bebc170e6
shizukani-cp <shizukani.cp@gmail.com>
2026-01-02 12:08:35 +0900

    タブ関連の設定粛清したらめっちゃ消えた

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,9 @@ config.font_size = 16
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.default_prog = { 'tmux', 'new-session', '-A', '-s', 'main' }
 config.default_cwd = wezterm.home_dir .. "/scratch/"
+config.use_ime = true
+config.window_background_opacity = 0.85
+config.hide_tab_bar_if_only_one_tab = true
 
 if not config.keys then
     config.keys = {}
@@ -21,44 +24,6 @@ config.font = wezterm.font_with_fallback({
     'BitstromWera Nerd Font Mono',
     'Noto Sans CJK JP', --Japanese
 })
-
-config.use_ime = true
-config.window_background_opacity = 0.85
-config.hide_tab_bar_if_only_one_tab = true
-
-config.colors = {
-    tab_bar = {
-        inactive_tab_edge = "none",
-    },
-}
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-    local background = "#5c6d74"
-    local foreground = "#FFFFFF"
-    local edge_background = "none"
-
-
-    if tab.is_active then
-        background = "#ae8b2d"
-        foreground = "#FFFFFF"
-    end
-    local edge_foreground = background
-
-    local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-
-    return {
-        { Background = { Color = edge_background } },
-        { Foreground = { Color = edge_foreground } },
-        { Text = SOLID_LEFT_ARROW },
-        { Background = { Color = background } },
-        { Foreground = { Color = foreground } },
-        { Text = title },
-        { Background = { Color = edge_background } },
-        { Foreground = { Color = edge_foreground } },
-        { Text = SOLID_RIGHT_ARROW },
-    }
-end)
 
 wezterm.on('window-config-reloaded', function(window, pane)
     window:maximize()
