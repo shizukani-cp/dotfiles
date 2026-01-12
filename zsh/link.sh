cc2bf9402cd3cf648699617dc05d3df08e315010
shizukani-cp <shizukani.cp@gmail.com>
2026-01-12 15:13:05 +0900

    よく使う自作のやつらを一文字コマンドで別ファイルに切り出した

--- zsh/link.sh
+++ zsh/link.sh
@@ -2,8 +2,7 @@ DOTFILES_PATH="$HOME/workspace/dotfiles"
 
 if [ $(basename $(pwd -P)) = "dotfiles" ]; then
   ln -sf "$DOTFILES_PATH/nvim" "$HOME/.config/"
-  ln -sf "$DOTFILES_PATH/zsh/.shfunc" "$HOME/.shfunc"
-  ln -sf "$DOTFILES_PATH/zsh/.shopts" "$HOME/.shopts"
+  ln -sf "$DOTFILES_PATH/zsh" "$HOME/.config/"
   ln -sf "$DOTFILES_PATH/tmux/" "$HOME/.config/"
   ln -sf "$DOTFILES_PATH/foot/" "$HOME/.config/"
   ln -sf "$DOTFILES_PATH/.gitconfig" "$HOME/.gitconfig"
