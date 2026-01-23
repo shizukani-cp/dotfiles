04262d76630c442dc5ed745de088b2d4b5e93de5
shizukani-cp <shizukani.cp@gmail.com>
2026-01-23 15:40:45 +0900

    なんとなくある程度link.shをnix化した

--- home.nix
+++ home.nix
@@ -15,5 +15,14 @@
     uv
     deno
   ];
+  home.file = {
+    ".config/nvim/".source = ./nvim;
+    ".config/zsh/".source = ./zsh;
+    ".config/tmux/".source = ./tmux;
+    ".config/foot/".source = ./foot;
+    ".gemini/".source = ./gemini;
+    ".gitconfig".source = ./.gitconfig;
+    ".poshthemes/my-theme.omp.json".source = ./zsh/my-theme.omp.json;
+  };
   programs.home-manager.enable = true;
 }
--- zsh/link.sh
+++ zsh/link.sh
@@ -1,15 +1,7 @@
 DOTFILES_PATH="$HOME/workspace/dotfiles"
 
 if [ $(basename $(pwd -P)) = "dotfiles" ]; then
-  ln -sf "$DOTFILES_PATH/nvim" "$HOME/.config/"
-  ln -sf "$DOTFILES_PATH/zsh" "$HOME/.config/"
-  ln -sf "$DOTFILES_PATH/tmux/" "$HOME/.config/"
-  ln -sf "$DOTFILES_PATH/foot/" "$HOME/.config/"
   ln -sf "$DOTFILES_PATH/" "$HOME/.config/home-manager"
-  ln -sf "$DOTFILES_PATH/.gitconfig" "$HOME/.gitconfig"
   ln -sf "$DOTFILES_PATH/zsh/link.sh" "$HOME/.local/bin/link.sh"
   ln -sf "$DOTFILES_PATH/zsh/input.sh" "$HOME/.local/bin/input.sh"
-  ln -sf "$DOTFILES_PATH/zsh/my-theme.omp.json" "$HOME/.poshthemes/my-theme.omp.json"
-  ln -sf "$DOTFILES_PATH/gemini/settings.json" "$HOME/.gemini/settings.json"
-  ln -sf "$DOTFILES_PATH/gemini/GEMINI.md" "$HOME/.gemini/GEMINI.md"
 fi
