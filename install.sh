2cb7871651fc1c3cf2a2678952ddb79625719168
shizukani-cp <shizukani.cp@gmail.com>
2026-01-25 18:42:52 +0900

    おまえの存在意義なくなったから粛清

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ curl --proto '=https' --tlsv1.2 -sSfL https://install.determinate.systems/nix | 
 
 sudo apt-get update
 
-sudo snap install tetris-thefenriswolf # tetris
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
