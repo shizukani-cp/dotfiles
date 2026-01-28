a685f8f5285e07ae5e0d10f00d720bd8e362bb7d
shizukani-cp <shizukani.cp@gmail.com>
2026-01-28 16:23:08 +0900

    keydをnix管理するのは厳しそうだからinstall.shをインストールスクリプトに追加

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/install.sh | zsh
 curl --proto '=https' --tlsv1.2 -sSfL https://install.determinate.systems/nix | sh -s -- install --determine-intel-64 --no-confirm # nix
 
+git clone https://github.com/rvaiya/keyd
+cd keyd
+make && sudo make install
+sudo systemctl enable --now keyd
+
 mkdir ~/.diary
 git clone https://github.com/shizukani-cp/diary.git ~/.diary
 
