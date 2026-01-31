07235718adc9ab6322d0cd3671a8dc3bb82217e2
shizukani-cp <shizukani.cp@gmail.com>
2026-01-31 09:51:13 +0900

    inactiveも設定できるんだ

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -52,6 +52,14 @@ local function config()
                 "location",
             },
         },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+        },
     })
 end
 
