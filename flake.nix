1189e29b6c7d5c6b126c51367008f1634f74e5f7
shizukani-cp <shizukani.cp@gmail.com>
2026-02-05 16:08:59 +0900

    home-manager導入

--- flake.nix
+++ flake.nix
@@ -1,29 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-gl.url = "github:nix-community/nixGL";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, nix-gl, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      unstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
     in {
-      homeConfigurations."shizukani-cp" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        extraSpecialArgs = {
-          inherit unstable inputs;
-          nixGL = nix-gl.packages.${system}.nixGLDefault;
-        };
-        modules = [ ./home.nix ];
+      nixosConfigurations."shizukani-cp" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.shizukani-cp = import ./home.nix;
+          }
+        ];
       };
     };
 }
--- home.nix
+++ home.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, unstable, nixGL, inputs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   home.username = "shizukani-cp";
   home.homeDirectory = "/home/shizukani-cp";
   home.stateVersion = "25.11";
-  home.packages = (with pkgs; [
+  home.packages = with pkgs; [
     bat
     bottom
     deno
@@ -25,76 +25,6 @@
     tmux
     uv
     zsh
-  ]) ++ [
-    unstable.theclicker
-    (pkgs.writeShellScriptBin "nyxt" ''
-      export LD_LIBRARY_PATH=${pkgs.mesa.drivers}/lib:${pkgs.libGL}/lib:$LD_LIBRARY_PATH
-      export WEBKIT_WEB_PROCESS_SANDBOX=0
-      exec ${pkgs.nyxt}/bin/nyxt "$@"
-    '')
   ];
-  home.file = {
-    ".gemini/".source = ./gemini;
-    ".gitconfig".source = ./.gitconfig;
-  };
-  xdg.enable = true;
-  xdg.configFile = {
-    "nvim" = {
-      source = ./nvim;
-    };
-    "foot" = {
-      source = ./foot;
-    };
-    "nyxt" = {
-      source = ./nyxt;
-    };
-    "zsh" = {
-      source = ./zsh;
-    };
-  };
-  home.sessionVariables = {
-    DOTFILES_DIR = "${config.home.sessionVariables.PROJECTS_DIR}/dotfiles";
-    EDITOR = "nvim";
-    LANG = "ja_JP.UTF-8";
-    LESSCHARSET = "utf-8";
-    PATH = "$HOME/.local/bin:$PATH";
-    PROJECTS_DIR = "${config.home.homeDirectory}/workspace";
-  };
-  programs.tmux = {
-    enable = true;
-    extraConfig = builtins.readFile ./tmux/tmux.conf;
-  };
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      rm = "gio trash";
-      tree = "tree --gitignore";
-    };
-    initContent = ''
-      fpath=(~/.config/zsh/functions $fpath)
-      autoload -Uz $(ls ~/.config/zsh/functions)
-      if [ -n "$TMUX" ] && [ -z "$NVIM" ]; then
-          if [ "$PWD" = "$HOME" ]; then
-              s
-          fi
-      fi
-    '';
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "gh"
-        "rust"
-        "uv"
-      ];
-    };
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-  };
-  programs.oh-my-posh = {
-    enable = true;
-    enableZshIntegration = true;
-    configFile = ./zsh/my-theme.omp.json;
-  };
   programs.home-manager.enable = true;
 }
