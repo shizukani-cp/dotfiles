6d89137ea00c381457770fefe03e9c126d6b22cd
shizukani-cp <shizukani.cp@gmail.com>
2026-02-05 15:57:09 +0900

    超最低限セット

--- flake.nix
+++ flake.nix
@@ -1,28 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-gl.url = "github:nix-community/nixGL";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, nix-gl, home-manager, ... } @ inputs:
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
     let
       system = "x86_64-linux";
-      unstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
     in {
       homeConfigurations."shizukani-cp" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${system};
-        extraSpecialArgs = {
-          inherit unstable inputs;
-          nixGL = nix-gl.packages.${system}.nixGLDefault;
-        };
         modules = [ ./home.nix ];
       };
     };
