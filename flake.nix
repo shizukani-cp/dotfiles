54df6a73c85ab415e400ff7132505d0c8b2b269c
shizukani-cp <shizukani.cp@gmail.com>
2026-02-05 16:58:05 +0900

    home-manager側が最小限のhome-manager連携

--- flake.nix
+++ flake.nix
@@ -1,17 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations."shizukani-cp" = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [
-          ./configuration.nix
-        ];
+        modules = [ ./configuration.nix ];
+      };
+
+      homeConfigurations."shizukani-cp" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
 }
