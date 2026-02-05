2ee3c7eb639197f85193e1059127d3a4c37a4ffe
shizukani-cp <shizukani.cp@gmail.com>
2026-02-05 15:59:00 +0900

    Add configuration.nix

---
+++ coniguration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
+
+  networking.hostName = "shizukani-cp";
+  networking.networkmanager.enable = true;
+
+  i18n.defaultLocale = "ja_JP.UTF-8";
+  console.keyMap = "jp106";
+
+  services.xserver = {
+    enable = true;
+    xkb.layout = "jp";
+  };
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  users.users.shizukani-cp = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [ git vim wget ];
+  programs.zsh.enable = true;
+
+  system.stateVersion = "25.11";
+}
