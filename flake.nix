95b501c189983c1909b3ab3e00f2187f53fe1fd8
shizukani-cp <shizukani.cp@gmail.com>
2026-02-05 16:04:47 +0900

    flake.nixがhome-manager用のままだった

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
-  outputs = { nixpkgs, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
     in {
-      homeConfigurations."shizukani-cp" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ ./home.nix ];
+      nixosConfigurations."shizukani-cp" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
       };
     };
 }
