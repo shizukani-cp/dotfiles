9df65766c373fb5904ca05e0eec524d067ec93a3
shizukani-cp <shizukani.cp@gmail.com>
2026-02-08 08:54:03 +0900

    インデントキモない?

--- home.nix
+++ home.nix
@@ -127,13 +127,13 @@
   programs.plasma = {
     enable = true;
     shortcuts = {
-    "kwin" = {
-      "Walk Through Windows" = "Alt+Tab";
-      "Walk Through Windows (Reverse)" = "Alt+Shift+Tab";
-      "Walk Through Windows Alternative" = [ ];
-      "Overview" = "Meta+Tab";
+      "kwin" = {
+        "Walk Through Windows" = "Alt+Tab";
+        "Walk Through Windows (Reverse)" = "Alt+Shift+Tab";
+        "Walk Through Windows Alternative" = [ ];
+        "Overview" = "Meta+Tab";
+      };
     };
-  };
     panels = [{
       location = "left";
       lengthMode = "fill";
