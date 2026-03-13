e1f33e7ca686e068dfd93a3f00a36a8dbfd279b4
shizukani-cp <shizukani.cp@gmail.com>
2026-03-13 15:09:08 +0900

    実体にそぐわぬもの?粛清粛清!

--- install.sh
+++ install.sh
@@ -2,15 +2,4 @@
 # curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/install.sh | zsh
 curl --proto '=https' --tlsv1.2 -sSfL https://install.determinate.systems/nix | sh -s -- install --determine-intel-64 --no-confirm # nix
 
-home-manager switch --flake .#shizukani-cp
-
-git clone https://github.com/rvaiya/keyd
-cd keyd
-make && sudo make install
-sudo systemctl enable --now keyd
-
-mkdir ~/.diary
-git clone https://github.com/shizukani-cp/diary.git ~/.diary
-
-gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+sudo nixos-rebuild switch --flake .#shizukani-cp
