221f6eae1522f7b389ab410c70706680287dc975
shizukani-cp <shizukani.cp@gmail.com>
2026-03-13 15:34:23 +0900

    環境系を入れる

---
+++ modules/env.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+{
+  home.username = "shizukani-cp";
+  home.homeDirectory = "/home/shizukani-cp";
+  home.stateVersion = "25.11";
+  home.pointerCursor = {
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+  home.file = {
+    ".gitconfig".source = ../.gitconfig;
+    ".local/share/skk/SKK-JISYO.L".source = "${pkgs.skkDictionaries.l}/share/skk/SKK-JISYO.L";
+  };
+  xdg.enable = true;
+  xdg.configFile = {
+    "nvim" = {
+      source = ../nvim;
+    };
+    "zsh" = {
+      source = ../zsh;
+    };
+  };
+  home.sessionVariables = {
+    DOTFILES_DIR = "${config.home.sessionVariables.PROJECTS_DIR}/dotfiles";
+    EDITOR = "nvim";
+    LANG = "ja_JP.UTF-8";
+    LESSCHARSET = "utf-8";
+    PATH = "$HOME/.local/bin:$PATH";
+    PROJECTS_DIR = "${config.home.homeDirectory}/workspace/github.com/shizukani-cp";
+  };
+}
