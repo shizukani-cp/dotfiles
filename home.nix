9d45e348beec70968b0b3be5fc6f0e30dfd73c87
shizukani-cp <shizukani.cp@gmail.com>
2026-03-13 15:04:59 +0900

    tmuxをnix化!この勢いで全てをnixにしてやる…

--- home.nix
+++ home.nix
@@ -103,7 +103,62 @@
   };
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ./tmux/tmux.conf;
+    plugins = with pkgs.tmuxPlugins; [
+      resurrect
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-save-interval '15'
+          set -g @continuum-restore 'on'
+        '';
+      }
+      vim-tmux-navigator
+    ];
+    shortcut = "a";
+    keyMode = "vi";
+    mouse = true;
+    terminal = "screen-256color";
+    extraConfig = ''
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      set -g status-position top
+      set -g status-justify left
+      set -g status-style bg=default,fg=white
+      set -g status-left ""
+      set -g status-left-length 0
+      setw -g window-status-format "#[fg=#5c6d74,bg=default]#[fg=#FFFFFF,bg=#5c6d74] #I:#W #[fg=#5c6d74,bg=default]"
+      setw -g window-status-current-format "#[fg=#ae8b2d,bg=default]#[fg=#FFFFFF,bg=#ae8b2d,bold] #I:#W #[fg=#ae8b2d,bg=default]"
+      setw -g window-status-separator ""
+      setw -g window-status-separator ""
+      set -g status-right-length 50
+      set -g status-right "#[bg=blue,fg=black,bold] #S #[default] #[fg=green]CPU:#(top -bn1 | grep 'Cpu(s)' | awk '{print $2}')%% #[fg=white]| #[fg=cyan]MEM:#(free -m | awk '/Mem:/ { printf(\"%.1f%%\", $3/$2*100) }') "
+      set -g status-interval 1
+
+      set -g status-keys vi
+      set -g default-command ""
+      unbind C-b
+
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
+      bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "pbcopy"
+      bind -n M-Left previous-window
+      bind -n M-Right next-window
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded!"
+      bind -n M-1 select-window -t 1
+      bind -n M-2 select-window -t 2
+      bind -n M-3 select-window -t 3
+      bind -n M-4 select-window -t 4
+      bind -n M-5 select-window -t 5
+      bind -n M-6 select-window -t 6
+      bind -n M-7 select-window -t 7
+      bind -n M-8 select-window -t 8
+      bind -n M-9 select-window -t 9
+      bind -n M-0 select-window -t 0
+    '';
   };
   programs.zsh = {
     enable = true;
