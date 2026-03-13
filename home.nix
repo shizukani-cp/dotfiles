41a869fc84737bc9025550b71184bc36d508bf91
shizukani-cp <shizukani.cp@gmail.com>
2026-03-13 13:04:33 +0900

    braveを書いておく

--- home.nix
+++ home.nix
@@ -142,6 +142,32 @@
     enableZshIntegration = true;
     configFile = ./zsh/my-theme.omp.json;
   };
+  programs.brave = {
+    enable = true;
+    extensions = [
+      "bhghoamapcdpbohphigoooaddinpkbai" # Authenticator
+      "cpedeojecpbkcomgcolphimkjdnikbck" # Auto Clicker
+      "mfmpnnhnknadgoafpindapobelgkblbk" # Cat-In-Tab
+      "clldacgmdnnanihiibdgemajcfkmfhia" # Color Picker for Chrome
+      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+      "ponfpcnoihfmfllpaingbgckeeldkhle" # Enhancer for YouTube
+      "godiecgffnchndlihlpaajjcplehddca" # GhostText
+      "ghbmnnjooekpmoecnnnilnnbdlolhkhi" # Google オフライン ドキュメント
+      "neebplgakaahbhdphmkckjjcegoiijjo" # Keepa
+      "ckkdlimhmcjmikdlpkmbgfkaikojcbjk" # Markdown Viewer
+      "ioijepjbllchodiajdakejdbjmdgggoj" # PLaMo Translate
+      "ghmbeldphafepmbegfdlkpapadhbakde" # Proton Pass
+      "jplgfhpmjnbigmhklmmbgecoobifkmpa" # Proton VPN
+      "gmloihcgbhbonllenincdakeijmikcne" # QR Scanner
+      "ldgfbffkinooeloadekpmfoklnobpien" # Raindrop.io
+      "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
+      "pnjaodmkngahhkoihejjehlcdlnohgmp" # RSS Feed Reader
+      "fjnbnpbmkenffdnngjfgmeleoegfcffe" # Stylish
+      "pncfbmialoiaghdehhbnbhkkgmjanfhe" # uBlacklist
+      "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
+      "gabfmnliflodkdafenbcpjdlppllnemd" # 画像を JPG/PNG/WebP として保存
+    ];
+  };
   services.mpd = {
     enable = true;
     musicDirectory = "${config.home.homeDirectory}/Music/bgm";
