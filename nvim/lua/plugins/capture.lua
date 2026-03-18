57d586054cf3f233b5c7e8e4cc84d4b6822f07bc
shizukani-cp <shizukani.cp@gmail.com>
2026-03-18 14:28:06 +0900

    これわざわざCaptureしなくてもいけるぞ

--- nvim/lua/plugins/capture.lua
+++ nvim/lua/plugins/capture.lua
@@ -4,11 +4,4 @@ return function(manager)
         url = "https://github.com/tyru/capture.vim",
     })
     manager:lazyload_event("CmdLineEnter", "capture.vim")
-    manager:lazyload_key(
-        "n",
-        "<Leader>em",
-        "<Cmd>Capture messages<Cr>",
-        "capture.vim",
-        { noremap = true, silent = true, desc = "Messages" }
-    )
 end
