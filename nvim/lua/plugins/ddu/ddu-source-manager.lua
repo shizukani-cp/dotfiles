e49d258ad7e0885d09af5822350253ca34a9c1a1
shizukani-cp <shizukani.cp@gmail.com>
2026-03-18 16:42:39 +0900

    プラグインに合わせないとな!

--- nvim/lua/plugins/ddu/ddu-source-manager.lua
+++ nvim/lua/plugins/ddu/ddu-source-manager.lua
@@ -1,8 +1,13 @@
+local function config(manager)
+    require("manager.ddu-source").setup(manager)
+end
+
 return function(manager)
     manager:add({
         id = "ddu-source-manager",
         url = "https://github.com/shizukani-cp/ddu-source-manager",
         dev = false,
         dir = require("utils.local_plugin_path")("ddu-source-manager"),
+        config = config,
     })
 end
