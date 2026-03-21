6d5c08f8fe5ec11c08b54cd58a691d6284fd3137
shizukani-cp <shizukani.cp@gmail.com>
2026-03-21 13:33:02 +0900

    完全に表示するだけ

--- nvim/lua/vist/core/init.lua
+++ nvim/lua/vist/core/init.lua
@@ -11,3 +11,26 @@
 ---@field list fun(): Vist.Item[]
 ---@field parse? fun(lines: string[]): Vist.Action<any>[]
 ---@field do_action? fun(action: Vist.Action<any>)
+
+local M = {}
+
+---@param adapter Vist.Adapter
+function M.open(adapter)
+    local items = adapter.list()
+    local lines = {}
+
+    for _, item in ipairs(items) do
+        local text = item.display or tostring(item.id) or ""
+        table.insert(lines, text)
+    end
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    vim.api.nvim_set_current_buf(buf)
+
+    vim.bo[buf].modified = false
+    vim.bo[buf].buftype = "acwrite"
+    vim.bo[buf].bufhidden = "hide"
+end
+
+return M
