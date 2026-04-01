ecbfe14758ac14b6dff189c2497b1df20840046e
shizukani-cp <shizukani.cp@gmail.com>
2026-04-02 05:27:10 +0900

    要らんやつら一通り消すか

--- nvim/lua/plugins/deps/promise-async.lua
+++
@@ -1,6 +0,0 @@
-return function(manager)
-    manager:add({
-        id = "promise-async",
-        url = "https://github.com/kevinhwang91/promise-async",
-    })
-end
--- nvim/lua/plugins/nvim-ghost.lua
+++
@@ -1,7 +0,0 @@
-return function(manager)
-    manager:add({
-        id = "nvim-ghost",
-        url = "https://github.com/subnut/nvim-ghost.nvim",
-    })
-    manager:lazyload_event("SafeState", "nvim-ghost")
-end
