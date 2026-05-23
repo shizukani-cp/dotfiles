e376ea64e42eb9b3dd76c4abf3bfd8a57e340648
shizukani-cp <shizukani.cp@gmail.com>
2026-05-23 14:43:20 +0900

    かっこ良くなったことを願う

--- modules/sway.nix
+++ modules/sway.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd sway --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red";
         user = "greeter";
       };
     };
