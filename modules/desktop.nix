78436d5e2a75c6a21eec69a4c50701960a6ad084
shizukani-cp <shizukani.cp@gmail.com>
2026-05-23 15:31:33 +0900

    rofiをTokyo nightにした

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ config, ... }:
+let
+  inherit (config.lib.formats.rasi) mkLiteral;
+in
 {
   programs.wlogout = {
     enable = true;
@@ -43,6 +46,134 @@
   };
   programs.rofi = {
     enable = true;
+    theme = {
+      "*" = {
+        bg = mkLiteral "#24283b";
+        hv = mkLiteral "#9274ca";
+        primary = mkLiteral "#C5C8C6";
+        ug = mkLiteral "#0B2447";
+        font = "Monospace 11";
+        background-color = mkLiteral "@bg";
+        border = mkLiteral "0px";
+        kl = mkLiteral "#7aa2f7";
+        black = mkLiteral "#000000";
+        transparent = mkLiteral "rgba(46,52,64,0)";
+      };
+
+      "window" = {
+        width = 700;
+        orientation = mkLiteral "horizontal";
+        location = mkLiteral "center";
+        anchor = mkLiteral "center";
+        transparency = "screenshot";
+        border-color = mkLiteral "@primary";
+        border = mkLiteral "3px";
+        border-radius = mkLiteral "6px";
+        spacing = 0;
+        children = map mkLiteral [ "mainbox" ];
+      };
+
+      "mainbox" = {
+        spacing = 0;
+        children = map mkLiteral [
+          "inputbar"
+          "message"
+          "listview"
+        ];
+      };
+
+      "inputbar" = {
+        color = mkLiteral "@kl";
+        padding = mkLiteral "11px";
+        border = mkLiteral "0 0 2px 0";
+        border-color = mkLiteral "@primary";
+      };
+
+      "message" = {
+        padding = 0;
+      };
+
+      "textbox" = {
+        color = mkLiteral "@kl";
+        padding = mkLiteral "10px";
+      };
+
+      "entry, prompt, case-indicator" = {
+        text-font = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+      };
+
+      "entry" = {
+        cursor = mkLiteral "pointer";
+      };
+
+      "prompt" = {
+        margin = mkLiteral "0px 5px 0px 0px";
+      };
+
+      "listview" = {
+        layout = mkLiteral "vertical";
+        padding = mkLiteral "8px";
+        lines = 12;
+        columns = 1;
+        dynamic = false;
+      };
+
+      "element" = {
+        padding = mkLiteral "2px";
+        vertical-align = 1;
+        color = mkLiteral "@kl";
+        font = mkLiteral "inherit";
+      };
+
+      "element-text" = {
+        background-color = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+      };
+
+      "element selected.normal" = {
+        color = mkLiteral "@black";
+        background-color = mkLiteral "@hv";
+      };
+
+      "element normal active" = {
+        background-color = mkLiteral "@hv";
+        color = mkLiteral "@black";
+      };
+
+      "element-text, element-icon" = {
+        background-color = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+      };
+
+      "element normal urgent" = {
+        background-color = mkLiteral "@primary";
+      };
+
+      "element selected active" = {
+        background = mkLiteral "@hv";
+        foreground = mkLiteral "@bg";
+      };
+
+      "button" = {
+        padding = mkLiteral "6px";
+        color = mkLiteral "@primary";
+        horizontal-align = mkLiteral "0.5";
+
+        border = mkLiteral "2px 0px 2px 2px";
+        border-radius = mkLiteral "4px 0px 0px 4px";
+        border-color = mkLiteral "@primary";
+      };
+
+      "button selected normal" = {
+        border = mkLiteral "2px 0px 2px 2px";
+        border-color = mkLiteral "@primary";
+      };
+
+      "scrollbar" = {
+        enabled = true;
+      };
+    };
     extraConfig = {
       modi = "combi";
       show-icons = true;
