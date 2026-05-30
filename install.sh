4bc0c3a44af040d08e75ef350301e76c862c8ffe
shizukani-cp <shizukani.cp@gmail.com>
2026-05-30 14:54:51 +0900

    お前いらねえだろ

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-# curl -sSf https://raw.githubusercontent.com/shizukani-cp/dotfiles/main/install.sh | zsh
-curl --proto '=https' --tlsv1.2 -sSfL https://install.determinate.systems/nix | sh -s -- install --determine-intel-64 --no-confirm # nix
-
-sudo nixos-rebuild switch --flake .#shizukani-cp
