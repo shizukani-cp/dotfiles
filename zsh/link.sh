e71bf91d35b410198e7294cb155ae33e96248982
shizukani-cp <shizukani.cp@gmail.com>
2026-05-30 14:02:48 +0900

    お前の存在意義を感じられないから粛清

--- zsh/link.sh
+++
@@ -1,7 +0,0 @@
-DOTFILES_PATH="$HOME/workspace/dotfiles"
-
-if [ $(basename $(pwd -P)) = "dotfiles" ]; then
-  ln -sf "$DOTFILES_PATH/" "$HOME/.config/home-manager"
-  ln -sf "$DOTFILES_PATH/zsh/link.sh" "$HOME/.local/bin/link.sh"
-  ln -sf "$DOTFILES_PATH/zsh/input.sh" "$HOME/.local/bin/input.sh"
-fi
