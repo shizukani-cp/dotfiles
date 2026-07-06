940179af9bc02b2360cf9a234a430ca74759e4ce
shizukani-cp <shizukani.cp@gmail.com>
2026-07-06 16:25:08 +0900

    タイピングっぽいことやりたいから分割する

--- nvim/lua/utils/example_file.lua
+++ nvim/lua/utils/example_file.lua
@@ -1,12 +1,16 @@
 local M = {}
 
 local function _open_scratch_buffer_with_cursor(lines)
-    local buf = vim.api.nvim_create_buf(false, true)
+    local sc_buf = vim.api.nvim_create_buf(false, true)
     local win = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(win, buf)
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    vim.api.nvim_win_set_buf(win, sc_buf)
+    local ex_buf = vim.api.nvim_create_buf(false, true)
+    vim.cmd([[vsplit]])
+    local win2 = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win2, ex_buf)
+    vim.api.nvim_buf_set_lines(ex_buf, 0, -1, false, lines)
+    vim.api.nvim_buf_set_name(ex_buf, "[Scratch]")
     vim.api.nvim_win_set_cursor(win, { 1, 0 })
-    vim.api.nvim_buf_set_name(buf, "[Scratch]")
 end
 
 M.open_example = function()
