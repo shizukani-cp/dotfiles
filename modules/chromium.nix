059565b2d4ac53b2a22b7e7ba89e043f5eada4cc
shizukani-cp <shizukani.cp@gmail.com>
2026-08-01 05:09:55 +0900

    サブブラウザにこんなに拡張機能は要りません!

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -11,26 +11,11 @@
     enable = true;
     extensions = [
       "bhghoamapcdpbohphigoooaddinpkbai" # Authenticator
-      "cpedeojecpbkcomgcolphimkjdnikbck" # Auto Clicker
-      "mfmpnnhnknadgoafpindapobelgkblbk" # Cat-In-Tab
       "clldacgmdnnanihiibdgemajcfkmfhia" # Color Picker for Chrome
-      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-      "ponfpcnoihfmfllpaingbgckeeldkhle" # Enhancer for YouTube
-      "godiecgffnchndlihlpaajjcplehddca" # GhostText
       "ghbmnnjooekpmoecnnnilnnbdlolhkhi" # Google オフライン ドキュメント
-      "neebplgakaahbhdphmkckjjcegoiijjo" # Keepa
-      "ckkdlimhmcjmikdlpkmbgfkaikojcbjk" # Markdown Viewer
-      "ioijepjbllchodiajdakejdbjmdgggoj" # PLaMo Translate
       "ghmbeldphafepmbegfdlkpapadhbakde" # Proton Pass
       "jplgfhpmjnbigmhklmmbgecoobifkmpa" # Proton VPN
       "gmloihcgbhbonllenincdakeijmikcne" # QR Scanner
-      "ldgfbffkinooeloadekpmfoklnobpien" # Raindrop.io
-      "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
-      "pnjaodmkngahhkoihejjehlcdlnohgmp" # RSS Feed Reader
-      "fjnbnpbmkenffdnngjfgmeleoegfcffe" # Stylish
-      "pncfbmialoiaghdehhbnbhkkgmjanfhe" # uBlacklist
-      "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
-      "gabfmnliflodkdafenbcpjdlppllnemd" # 画像を JPG/PNG/WebP として保存
     ];
     extraOpts = {
       "PasswordManagerEnabled" = false;
@@ -39,16 +24,8 @@
         "ghmbeldphafepmbegfdlkpapadhbakde" = {
           "toolbar_pin" = "force_pinned";
         };
-        # Dark Reader
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" = {
-          "toolbar_pin" = "force_pinned";
-        };
         # Color Picker for Chrome
         "clldacgmdnnanihiibdgemajcfkmfhia" = {
-          "toolbar_pin" = "force_pinned";
-        };
-        # PLaMo Translate
-        "ioijepjbllchodiajdakejdbjmdgggoj" = {
           "toolbar_pin" = "force_pinned";
         };
         # Authenticator
