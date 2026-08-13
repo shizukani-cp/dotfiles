03c8f711e7cb8c96633bb698bdef7b8a4b5aadee
shizukani-cp <shizukani.cp@gmail.com>
2026-08-13 15:35:54 +0900

    地味に昔消したはずのやり方してるな

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -1,32 +1,34 @@
+local function config()
+    require("conform").setup({
+        formatters_by_ft = {
+            lua = { "stylua" },
+            nix = { "nixfmt" },
+        },
+    })
+
+    vim.g.formatonsave = true
+    vim.keymap.set({ "n" }, "<Leader>ef", function()
+        if vim.g.formatonsave then
+            vim.g.formatonsave = false
+        else
+            vim.g.formatonsave = true
+        end
+    end, { noremap = true, silent = true, desc = "Toggle fonmat on save" })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+        pattern = "*",
+        callback = function(args)
+            if vim.g.formatonsave then
+                require("conform").format({ bufnr = args.buf })
+            end
+        end,
+    })
+end
+
 return function(manager)
     manager:add({
         id = "conform.nvim",
         url = "https://github.com/stevearc/conform.nvim",
-        config = function()
-            require("conform").setup({
-                formatters_by_ft = {
-                    lua = { "stylua" },
-                    nix = { "nixfmt" },
-                },
-            })
-
-            vim.g.formatonsave = true
-            vim.keymap.set({ "n" }, "<Leader>ef", function()
-                if vim.g.formatonsave then
-                    vim.g.formatonsave = false
-                else
-                    vim.g.formatonsave = true
-                end
-            end, { noremap = true, silent = true, desc = "Toggle fonmat on save" })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
-                pattern = "*",
-                callback = function(args)
-                    if vim.g.formatonsave then
-                        require("conform").format({ bufnr = args.buf })
-                    end
-                end,
-            })
-        end,
+        config = config,
     })
 end
