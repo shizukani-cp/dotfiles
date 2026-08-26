467f5ac0b88356c683a61781a76bde5e57a15f6d
shizukani-cp <shizukani.cp@gmail.com>
2026-08-26 11:58:51 +0900

    一応設定しといた

--- modules/cli.nix
+++ modules/cli.nix
@@ -317,6 +317,40 @@ in
   };
   programs.fastfetch = {
     enable = true;
+    settings = {
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "shell"
+        "display"
+        "de"
+        "wm"
+        "wmtheme"
+        "theme"
+        "icons"
+        "font"
+        "cursor"
+        "terminal"
+        "terminalfont"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "disk"
+        "localip"
+        "battery"
+        "poweradapter"
+        "locale"
+        "break"
+        "colors"
+
+      ];
+    };
   };
   programs.fd = {
     enable = true;
